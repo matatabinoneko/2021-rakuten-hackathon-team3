26c88f65c90e891d9f39b146893a1a412bdeb825
Katsumi-N <naya.katsumi@gmail.com>
2021-09-14 14:27:53 +0900

    add usergetapi and fix usercreateapi

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,12 @@
 from rest_framework import routers
 from django.urls import path
 from django.conf.urls import include
-from .views.user_view import CreateUserView
+from .views.user_view import CreateUserView, ListUserView, RetrieveUserView
 router = routers.DefaultRouter()
 
 urlpatterns = [
      path('', include(router.urls)),
-     path('create/', CreateUserView.as_view(), name='create'),
+     path('users/create/', CreateUserView.as_view(), name='create'),
+     path('users/<str:loginid>/', RetrieveUserView.as_view(), name='retrieve'),
+     path('users/', ListUserView.as_view(), name="list")
 ]
