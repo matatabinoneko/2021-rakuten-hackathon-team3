542ba84a6890262a25c79f485d44057ac4714074
matatabinoneko <matatabinoneko0721@gmail.com>
2021-09-14 22:51:14 +0900

    [frontend] make router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,34 @@
 import logo from "./logo.svg";
 import "./css/App.css";
+import RoutingPage from "pages/RoutingPage";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import TopPage from "pages/TopPage";
+import MockPage from "pages/MockPage";
+import Login from "pages/Login";
+import Registrationpage from "pages/Registrationpage";
 
 function App() {
-	return <TopPage />;
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/mock">
+					<MockPage />
+				</Route>
+				<Route path="/top">
+					<TopPage />
+				</Route>
+				<Route path="/registration">
+					<Registrationpage />
+				</Route>
+				<Route path="/signin">
+					<Login />
+				</Route>
+				<Route path="/">
+					<RoutingPage />
+				</Route>
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
