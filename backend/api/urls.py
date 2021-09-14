63f7e30df5c4108e1b0545de8195bc3b1be64d0f
syhsyh9696 <sunyuanhao123456@gmail.com>
2021-09-14 16:17:50 +0900

    [Backend] Update preferencetag into url file.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,13 @@
 from rest_framework import routers
 from django.urls import path
 from django.conf.urls import include
+
 from .views.user_view import CreateUserView
+from .views.preferencetag_view import PreferenceTagList
 router = routers.DefaultRouter()
 
 urlpatterns = [
      path('', include(router.urls)),
      path('create/', CreateUserView.as_view(), name='create'),
+     path('preferencetags/', PreferenceTagList.as_view(),)
 ]
