57b26745694eaf11f836371d5eb4f24dd545a19f
MOTCCi <3motochika@gmail.com>
2021-09-15 19:07:06 +0900

    [frontend] modify style of date

--- frontend/src/components/FriendItems.js
+++ frontend/src/components/FriendItems.js
@@ -7,16 +7,26 @@ import styles from "css/Friends.css"
 
 function FriendItems(props) {
 
-        const list = props.friends.map((friend,id) => (
+        
+
+
+        const list = props.friends.map((friend,id) => {
+
+            const birthday = friend.birthday.split('-')
+            const year = birthday[0]
+            const month = birthday[1].replace(/^0+/,'')
+            const day = birthday[2].replace(/^0+/,'')
+            return(
                 <li>
                     <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.jpg"/>
                     <div class="friend-birthday">
-                        <h3 class="friend-birthday-year">2021</h3>
-                    <h2 class="friend-birthday-month-date"> {friend.birtday}</h2>
+                        <h3 class="friend-birthday-year">{year}</h3>
+                    <h2 class="friend-birthday-month-date"> {month}/{day}</h2>
                     </div>
                     <h4 class="friend-name">{friend.username}</h4>
                 </li>
-            ));
+            )
+        });
 
     return (
         <div>
