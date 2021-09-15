75e02db2a224f92680aca4e2d4f67aaad0170b83
Katsumi-N <naya.katsumi@gmail.com>
2021-09-15 09:36:48 +0900

    add friendship relation api(create/delete)

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from django.conf.urls import include
 
 from .views.user_view import CreateUserView
-from .views.user_view import CreateUserView, ListUserView, RetrieveUserView
-
+from .views.user_view import CreateUserView, ListUserView, RetrieveUserView, FriendAPI
 from .views.preferencetag_view import PreferenceTagList
 
 from .views.wishlist_view import WishlistList, WishlistDetail
@@ -15,6 +14,7 @@ urlpatterns = [
      path('', include(router.urls)),
      path('users/create/', CreateUserView.as_view(), name='create'),
      path('users/<str:loginid>/', RetrieveUserView.as_view(), name='retrieve'),
+     path('users/<str:loginid>/friend/', FriendAPI.as_view()),
      path('users/', ListUserView.as_view(), name="list"),
      path('preferencetags/', PreferenceTagList.as_view(),),
      path('wishlists/', WishlistList.as_view(),),
