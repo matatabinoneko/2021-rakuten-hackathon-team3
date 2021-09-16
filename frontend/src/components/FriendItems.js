c16cd8f3d3bfe285a64266b20c91a8055c7caa2c
MOTCCi <3motochika@gmail.com>
2021-09-16 20:05:27 +0900

    [frontend] success get userid from friend card

--- frontend/src/components/FriendItems.js
+++ frontend/src/components/FriendItems.js
@@ -3,13 +3,17 @@ import "css/Friends.css"
 
 function FriendItems(props) {
 
+    const itemClickHandle = (loginid) => {
+        console.log(loginid)
+    }
+
         const list = props.friends.map((friend,id) => {
             const birthday = friend.birthday.split('-')
             const year = birthday[0]
             const month = birthday[1].replace(/^0+/,'')
             const day = birthday[2].replace(/^0+/,'')
             return(
-                <li>
+                <li onClick={() => itemClickHandle(friend.loginid)}>
                     <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.jpg"/>
                     <div className="friend-birthday">
                         <h3 className="friend-birthday-year">{year}</h3>
