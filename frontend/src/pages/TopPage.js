6b957c45f767742462a3a582b57eeffbb4c32149
Takumu Yoshiba <mukuta273.pooh@gmail.com>
2021-09-16 13:46:53 +0900

    added header.css

--- frontend/src/pages/TopPage.js
+++ frontend/src/pages/TopPage.js
@@ -6,6 +6,7 @@ import AddMyWishItemList from "components/AddMyWishItemList";
 import { useEffect, useState } from "react";
 import { getWishList } from "data/api/mock";
 import { ToastProvider } from "react-toast-notifications";
+import "css/TopPage.css"
 // import Auth from "./Auth";
 
 function TopPage() {
@@ -40,19 +41,21 @@ function TopPage() {
 			<ToastProvider>
 				<AddMyWishItemList search={search} />
 			</ToastProvider>
-			<div className="container">
-				{/* <div className="row justify-content-center">
-					<div className="col">
-						<AddMyWishItemList />
-					</div>
-				</div> */}
-				<div className="row justify-content-center">
-					<div className="col-md-3">
-						<FriendsList />
-					</div>
-					<div className="col-md-9">
-						<WishList items={wishItems} />
-						<TagsList items={tagItems} tagName="tagName" />
+			<div className="body">
+				<div className="container">
+					{/* <div className="row justify-content-center">
+						<div className="col">
+							<AddMyWishItemList />
+						</div>
+					</div> */}
+					<div className="row justify-content-center">
+						<div className="col-md-3">
+							<FriendsList />
+						</div>
+						<div className="col-md-9">
+							<WishList items={wishItems} />
+							<TagsList items={tagItems} tagName="tagName" />
+						</div>
 					</div>
 				</div>
 			</div>
